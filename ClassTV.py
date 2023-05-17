80933f4339be77b3da8ddba55a8fd32cc66aefa5
reniel8to3 <renielalmariomalabo@gmail.com>
2023-05-17 23:45:15 +0800

    pseudocode for classtv

---
+++ ClassTV.py
@@ -0,0 +1,13 @@
+#create class
+
+    #create tv attributes
+
+    #create on and off functions
+
+    #create channel function
+
+    #create set channel function 
+
+    #volume up and down
+
+    #channel up and down
