1dea20d9c12b9c63b4a9cc3bfea225dd83a5de78
reniel8to3 <renielalmariomalabo@gmail.com>
2023-05-17 23:56:36 +0800

    on and off + fixed indentions

--- ClassTV.py
+++ ClassTV.py
@@ -1,12 +1,17 @@
 #create class
 class TV:
-    #create tv attributes
- def __init__(self, channel, volumeLevel, on):
-        self.channel = channel
-        self.volumeLevel = volumeLevel
-        self.on = on
-    #create on and off functions
+#create tv attributes
+    def __init__(self, channel, volumeLevel, on):
+            self.channel = channel
+            self.volumeLevel = volumeLevel
+            self.on = on
+#create on and off functions
+    def turnOn(self):
+        self.on = True
 
+    def turnOff(self):
+        # turns off the TV
+        self.on = False
     #create channel function
 
     #create set channel function 
