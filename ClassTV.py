4a0f1ade6c863782808e6657a219cef4bbdd810f
reniel8to3 <renielalmariomalabo@gmail.com>
2023-05-18 00:00:20 +0800

    fixed some typos + channel function... its obvious i need to sleep

--- ClassTV.py
+++ ClassTV.py
@@ -1,14 +1,18 @@
 #create class
 class TV:
-    #create tv attributes
- def __init__(self, channel, volumeLevel, on):
-        self.channel = channel
-        self.volumeLevel = volumeLevel
-        self.on = on
-    #create on and off functions
-
+#create tv attributes
+    def __init__(self, channel, volumeLevel, on):
+            self.channel = channel
+            self.volumeLevel = volumeLevel
+            self.on = on
+#create on and off functions
+    def turnOn(self):
+        self.on = True
+    def turnOff(self):
+        self.on = False
     #create channel function
-
+    def getChannel(self):
+        return self.channel
     #create set channel function 
 
     #volume up and down
