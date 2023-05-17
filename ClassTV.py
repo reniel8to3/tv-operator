754fe8a7c82df3151687edd387af0c1f435372b5
reniel8to3 <renielalmariomalabo@gmail.com>
2023-05-17 23:52:39 +0800

    added class tv and self attributes

--- ClassTV.py
+++ ClassTV.py
@@ -1,7 +1,10 @@
 #create class
-
+class TV:
     #create tv attributes
-
+ def __init__(self, channel, volumeLevel, on):
+        self.channel = channel
+        self.volumeLevel = volumeLevel
+        self.on = on
     #create on and off functions
 
     #create channel function
