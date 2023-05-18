8398b93fb9af142dc8b50e5bd4eb55e6ced42bc9
reniel8to3 <renielalmariomalabo@gmail.com>
2023-05-18 19:45:13 +0800

    fixed print statements... forgot to edit the second statement before commiting

--- TestTV.py
+++ TestTV.py
@@ -6,11 +6,11 @@ def TestTV():
     tv1 = TV(1, 1, True)
     tv1.setChannel(30)
     tv1.setVolume(3)
-    print(f"You are now watching {tv1.getChannel()} on TV1 and its volume level is {tv1.getVolume()}")
+    print(f"You are now watching channel {tv1.getChannel()} on TV1 and its volume level is {tv1.getVolume()}")
 
     tv2 = TV(1, 1, True)
     tv2.setChannel(3)
     tv2.setVolume(2)
-    print(f"TV2 is set to channel  {tv2.getChannel()} and its volume level is {tv2.getVolume()}")
+    print(f"You are now watching channel {tv2.getChannel()} on TV and its volume level is {tv2.getVolume()}")
 
 TestTV()
